de7e95cd72532476c4683510be5edabef52afe0b
shitou <2832904090@qq.com>
2019-06-02 16:23:09 +0800

    edit README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# create-react-cms
+> Quickly create cms app based on react
+
+## Install
+
+```
+$ npm install create-react-cms
+```
+
+## Usage
+
+```
+create-react-cms <name> 
+```
+
+## Feature
+> 1. auto install react-router, mobx, react-intl, material-ui
+> 2. download templates of your repository
--- bin/index.js
+++ bin/index.js
@@ -2,103 +2,46 @@
 
 const fs = require('fs-extra')
 const program = require('commander')
-const lodash = require('lodash')
 const inquirer = require('inquirer')
-const chalk = require('chalk')
-const symbols = require('log-symbols')
 const Listr = require('listr')
-const execa = require('execa')
-const UpdaterRenderer = require('listr-update-renderer');
-/*async function installDependencies(steps) {
-  for (let step of steps) {
-    step.ora = ora(step.tip).start()
-
-    await execAsync(step.command)
-    step.ora.succeed()
-  }
-}*/
+const UpdaterRenderer = require('listr-update-renderer')
+const { exec, execSync } = require('child_process')
+const { promisify } = require('util')
+const execAsync = promisify(exec)
 
 program.version('1.0.0', '-v, --version')
-  .command('<name>')
+  .arguments('<name>')
   .action((name) => {
     inquirer.prompt([
       {
         type: 'confirm',
         name: 'existDir',
-        message: `${name}项目已存在，是否继续？`,
+        message: `Project ${name} already exists, want to continue?`,
         when () {
           return fs.existsSync(name)
         }
+      },
+      {
+        type: 'confirm',
+        name: 'multiLanguage',
+        message: 'Project need to support multiple languages?',
+        when (answers) {
+          return answers.existDir !== false
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'repository',
+        message: 'Please input templates repository?'
       }
     ]).then((answers) => {
-      /*const spinner = ora('正在下载模板...');
-      spinner.start();
-      download('http://xxxxxx:9999:HTML5/H5Template#master', name, {clone: true}, (err) => {
-        if(err){
-          spinner.fail();
-          console.log(symbols.error, chalk.red(err));
-        }else{
-          spinner.succeed();
-          const fileName = `${name}/package.json`;
-          const meta = {
-            name,
-            description: answers.description,
-            author: answers.author
-          }
-          if(fs.existsSync(fileName)){
-            const content = fs.readFileSync(fileName).toString();
-            const result = handlebars.compile(content)(meta);
-            fs.writeFileSync(fileName, result);
-          }
-          console.log(symbols.success, chalk.green('项目初始化完成'));
-        }
-      })*/
-      /*const createReactAppOra = ora('create react app').start()
-      const addRouter = ora('add react router').start()
-      const addMobx = ora('add mobx').start()
-      const addMaterialUI = ora('add material-ui').start()*/
-
-      /*execAsync(`npx create-react-app ${name}`)
-        .then(() => {
-          retd
-        })
-        .catch(err => {})*/
-      /*const commands = [
-        `npx create-react-app ${name} && cd ${name}`,
-        'npm install react-router-dom --save',
-        'npm install mobx mobx-react --save',
-        'npm install @material-ui/core --save'
-      ]*/
-      /*
-
-
-            const steps = [
-              {
-                command: `npx create-react-app ${name}`,
-                tip: 'create react app'
-              },
-              {
-                command: `cd ${name} && npm install react-router-dom --save`,
-                tip: 'add react router'
-              },
-              {
-                command: `cd ${name} && npm install mobx mobx-react --save`,
-                tip: 'add mobx'
-              },
-              {
-                command: `cd ${name} && npm install @material-ui/core --save`,
-                tip: 'add material-ui'
-              }
-            ]
-
-            execAsync()
-            execCommands(steps)*/
       if (answers.existDir === false) { return }
 
-      const tasks = new Listr([
+      const tasks = new Listr(
+        [
           {
             title: 'create react app',
-            task: () => execa(`npx create-react-app ${name}`)
+            task: () => execAsync(`npx create-react-app ${name}`)
           },
           {
             title: 'install dependencies',
@@ -106,35 +49,40 @@ program.version('1.0.0', '-v, --version')
               [
                 {
                   title: 'add router',
-                  task: () => execa.stdout('npm install react-router-dom --save', { cwd: name })
+                  task: () => execAsync('npm install react-router-dom --save', { cwd: name })
                 },
                 {
                   title: 'add mobx',
-                  task: () => execa.stdout('npm install mobx mobx-react --save', { cwd: name })
+                  task: () => execAsync('npm install mobx mobx-react --save', { cwd: name })
                 },
                 {
                   title: 'add material-ui',
-                  task: () => execa.stdout('npm install react-router-dom --save', { cwd: name })
+                  task: () => execAsync('npm install @material-ui/core --save', { cwd: name })
+                },
+                {
+                  title: 'add react-intl',
+                  enabled: () => answers.multiLanguage,
+                  task: () => execAsync('npm install react-intl --save', { cwd: name })
                 }
               ],
               {
-                concurrent: true,
-                renderer: UpdaterRenderer,
-                collapse: false
+                concurrent: true
               }
             )
           },
           {
             title: 'download template',
-            task: () => execa.stdout('git clone -b \'templates\' --single-branch --depth 1 https://github.com/shitouplus/create-cms-app.git templates')
-              .then(data => {
-                fs.removeSync('.git', { cwd: 'templates' })
-                fs.moveSync('templates', name)
-                return data
-              })
+            task: () => {
+              execSync(`git clone --depth 1 ${answers.repository} templates`, { cwd: name })
+              fs.copySync(`${name}/templates`, `${name}/src`, { filter: src => !src.includes('.git') })
+              fs.removeSync(`${name}/templates`)
+            }
           }
-        ]
-      )
+        ],
+        {
+          renderer: UpdaterRenderer,
+          collapse: false
+        })
 
       tasks.run().catch(err => {
         console.error(err)
